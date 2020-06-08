3346486cc098c54e057f8f59c0c5ec86c40dbb1f
shihab <echoshihab@gmail.com>
2020-06-07 21:01:29 -0400

    add shifts delete handler + api endpoint

--- API/Controllers/ShiftsController.cs
+++ API/Controllers/ShiftsController.cs
@@ -37,6 +37,12 @@ namespace API.Controllers
             return await _mediator.Send(command);
         }
 
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> Delete(Guid id)
+        {
+            return await _mediator.Send(new Delete.Command { Id = id });
+        }
+
 
 
     }
