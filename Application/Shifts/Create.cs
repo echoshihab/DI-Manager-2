eb27bb673d76161cc34e5cd5d34a3e4fd3f6b1bd
shihab <echoshihab@gmail.com>
2020-06-07 20:38:51 -0400

    add shifts create handler + api endpoint

---
+++ Application/Shifts/Create.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Shifts
+{
+    public class Create
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+            public DateTime Start { get; set; }
+            public DateTime End { get; set; }
+            public string License { get; set; }
+            public string Location { get; set; }
+            public string Room { get; set; }
+            public string Technologist { get; set; }
+            public string Modality { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly ApplicationDbContext _context;
+            public Handler(ApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var shift = new Shift
+                {
+                    Id = request.Id,
+                    Start = request.Start,
+                    End = request.End,
+                    License = request.License,
+                    Location = request.Location,
+                    Room = request.Room,
+                    Technologist = request.Technologist,
+                    Modality = request.Modality
+
+                };
+                _context.Shifts.Add(shift);
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if (success) return Unit.Value;
+
+                throw new Exception("Problem saving changes");
+            }
+        }
+    }
+}
