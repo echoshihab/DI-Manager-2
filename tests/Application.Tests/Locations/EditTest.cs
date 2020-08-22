6c50da07597ff54ed13fa3e7ddd26e1f9e4595d3
shihab <echoshihab@gmail.com>
2020-08-22 14:49:29 -0400

    add test for edit location

---
+++ tests/Application.Tests/Locations/EditTest.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Threading;
+using Application.Rooms;
+using AutoMapper;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace Application.Tests.Locations
+{
+    public class EditTest : TestBase
+    {
+        // private readonly IMapper _mapper;
+        // public EditTest()
+        // {
+        //     var mockMapper = new MapperConfiguration(cfg => { cfg.AddProfile(new MappingProfile()); });
+        //     _mapper = mockMapper.CreateMapper();
+        // }
+
+        [Fact]
+        public void Should_Edit_Location()
+        {
+            var context = GetDbContext();
+            var id = Guid.NewGuid();
+
+            context.Locations.Add(new Location
+            {
+                Id = id,
+                Name = "Test Location"
+            });
+
+            context.SaveChanges();
+
+
+            var sut = new Application.Locations.Edit.Handler(context);
+
+            var result = sut.Handle(
+                new Application.Locations.Edit.Command
+                {
+                    Id = id,
+                    Name = "Updated Location"
+                }, CancellationToken.None).Result;
+
+            var location = context.Locations.FirstOrDefaultAsync(x => x.Id == id).Result;
+
+            Assert.Equal("Updated Location", location.Name);
+
+        }
+
+
+
+    }
+}
