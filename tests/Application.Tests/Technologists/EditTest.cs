893837fc11d122162fc2a5203dcb5e71003b2874
shihab <echoshihab@gmail.com>
2020-09-08 20:38:23 -0400

    add test for updating technologist w/ new licenses

--- tests/Application.Tests/Technologists/EditTest.cs
+++ tests/Application.Tests/Technologists/EditTest.cs
@@ -11,7 +11,7 @@ namespace Application.Tests.Technologists
     public class EditTest : TestBase
     {
         [Fact]
-        public void Should_Edit_Technologist()
+        public void Should_Update_Technologist()
         {
             var context = GetDbContext();
 
@@ -45,13 +45,62 @@ namespace Application.Tests.Technologists
             Assert.Equal("Updated Technologist", editedTechnologist.Name);
             Assert.Equal("UT", editedTechnologist.Initial);
 
-
-
-
-
-
-
         }
 
+        [Fact]
+        public void Should_Update_Technologist_With_New_License()
+        {
+            var context = GetDbContext();
+
+            var modalityId = Guid.NewGuid();
+            context.Modalities.Add(new Domain.Modality { Id = modalityId, Name = "Test Modality", DisplayName = "TM" });
+            context.SaveChanges();
+
+            var technologistId = Guid.NewGuid();
+
+            context.Technologists.Add(new Domain.Technologist
+            {
+                Id = technologistId,
+                Name = "Test Technologist",
+                ModalityId = modalityId,
+                Initial = "TT",
+            });
+
+            var licenceId1 = Guid.NewGuid();
+            var licenseId2 = Guid.NewGuid();
+
+            context.Licenses.Add(new Domain.License { Id = licenceId1, Name = "License 1", DisplayName = "L1", ModalityId = modalityId });
+            context.Licenses.Add(new Domain.License { Id = licenseId2, Name = "License 2", DisplayName = "L2", ModalityId = modalityId });
+
+
+            context.SaveChanges();
+
+            var sut = new Edit.Handler(context);
+            var licenseIdList = new List<Guid> { licenceId1, licenseId2 };
+            var result = sut.Handle(new Edit.Command
+            {
+                Id = technologistId,
+                Name = "Updated Technologist",
+                Initial = "UT",
+                LicenseIdList = licenseIdList
+            }, CancellationToken.None).Result;
+
+            var editedTechnologist = context.Technologists.Include(x => x.Licenses).ThenInclude(x => x.License).FirstOrDefault(x => x.Id == technologistId);
+            var editedTechnologistLicense = editedTechnologist.Licenses.FirstOrDefault(x => x.LicenseId == licenceId1);
+
+            Assert.Equal("Updated Technologist", editedTechnologist.Name);
+            Assert.Equal("UT", editedTechnologist.Initial);
+            Assert.Equal(2, editedTechnologist.Licenses.Count);
+            Assert.NotNull(editedTechnologistLicense);
+            Assert.Equal("License 1", editedTechnologistLicense.License.Name);
+
+        }
     }
 }
+
+// [Fact]
+// public void Should_Update_Technologist_By_Removing_License()
+// {
+
+// }
+
