4818ae09f6d7c5e75c2967aca43c367bf1fcb104
shihab <echoshihab@gmail.com>
2020-09-24 17:36:29 -0400

    split test for license by requested vs unrequested

--- tests/Application.Tests/Licenses/ListTest.cs
+++ tests/Application.Tests/Licenses/ListTest.cs
@@ -17,36 +17,57 @@ namespace Application.Tests.Licenses
             _mapper = mockMapper.CreateMapper();
 
         }
+
         [Fact]
-        public void Should_List_Licenses()
+        public void Should_List_Licenses_By_Requested_Modality()
         {
 
             var context = GetDbContext();
 
-            var modalityId1 = Guid.NewGuid();
-            var modalityId2 = Guid.NewGuid();
-            context.Modalities.Add(new Domain.Modality { Id = modalityId1, Name = "Test Modality 1 " });
-            context.Modalities.Add(new Domain.Modality { Id = modalityId2, Name = "Test Modality 2" });
+
+            var modalityId = Guid.NewGuid();
+            context.Modalities.Add(new Domain.Modality { Id = modalityId, Name = "Test Modality 1 " });
             context.SaveChanges();
 
-
-            context.Licenses.Add(new Domain.License { Id = Guid.NewGuid(), Name = "Test License", DisplayName = "TL", ModalityId = modalityId1 });
-            context.Licenses.Add(new Domain.License { Id = Guid.NewGuid(), Name = "Test License 2", DisplayName = "TL2", ModalityId = modalityId1 });
-
-            context.Licenses.Add(new Domain.License { Id = Guid.NewGuid(), Name = "Test License 3", DisplayName = "TL3", ModalityId = modalityId2 });
+            context.Licenses.Add(new Domain.License { Id = Guid.NewGuid(), Name = "Test License", DisplayName = "TL", ModalityId = modalityId });
+            context.Licenses.Add(new Domain.License { Id = Guid.NewGuid(), Name = "Test License 2", DisplayName = "TL2", ModalityId = modalityId });
             context.SaveChanges();
 
             var sut = new List.Handler(context, _mapper);
 
-            var result = sut.Handle(new List.Query(modalityId1), CancellationToken.None).Result;
+            var result = sut.Handle(new List.Query(modalityId), CancellationToken.None).Result;
 
             var licenseExists = result.Any(x => x.Name == "Test License");
-            var licenseDoesNotExist = result.Any(x => x.Name == "Test License 3");
 
             Assert.Equal(2, result.Count);
             Assert.True(licenseExists);
-            Assert.False(licenseDoesNotExist);
 
+        }
+
+        [Fact]
+        public void Should_Not_Return_Unrequested_Modality_Licenses()
+        {
+
+            var context = GetDbContext();
+
+
+            var modalityId1 = Guid.NewGuid();
+            var modalityId2 = Guid.NewGuid();
+
+            context.Modalities.Add(new Domain.Modality { Id = modalityId1, Name = "Requested Modality" });
+            context.Modalities.Add(new Domain.Modality { Id = modalityId2, Name = "Unrequested Modality" });
+            context.SaveChanges();
+
+            context.Licenses.Add(new Domain.License { Id = Guid.NewGuid(), Name = "RM License", DisplayName = "RL", ModalityId = modalityId1 });
+            context.Licenses.Add(new Domain.License { Id = Guid.NewGuid(), Name = "UM License", DisplayName = "UL", ModalityId = modalityId2 });
+            context.SaveChanges();
+
+            var sut = new List.Handler(context, _mapper);
+            var result = sut.Handle(new List.Query(modalityId1), CancellationToken.None).Result;
+            var licenseExists = result.Any(x => x.Name == "UM License");
+
+            Assert.Equal(1, result.Count);
+            Assert.False(licenseExists);
         }
     }
 }
