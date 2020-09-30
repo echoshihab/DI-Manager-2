3c1ef48d21560f871e620d68854c1afbf0b62156
shihab <echoshihab@gmail.com>
2020-09-30 19:15:27 -0400

    test for create room with invalid location

--- tests/Application.Tests/Rooms/CreateTest.cs
+++ tests/Application.Tests/Rooms/CreateTest.cs
@@ -4,11 +4,13 @@ using Xunit;
 using Application.Rooms;
 using System.Threading;
 using Microsoft.EntityFrameworkCore;
+using Application.Errors;
 
 namespace Application.Tests.Rooms
 {
     public class CreateTest : TestBase
     {
+
         [Fact]
         public void Should_Create_Room()
         {
@@ -45,6 +47,33 @@ namespace Application.Tests.Rooms
             Assert.Equal("Test Room", room.Name);
 
 
+
+        }
+
+
+        [Fact]
+        public void Should_Fail_With_Invalid_Location()
+        {
+            var context = GetDbContext();
+            context.Locations.Add(new Location { Id = Guid.NewGuid(), Name = "Location In Db" });
+            context.SaveChanges();
+
+
+            var CreateCommandInvalidLocationId = new Create.Command
+            {
+                Id = Guid.NewGuid(),
+                Name = "Test Room",
+                LocationId = Guid.NewGuid()
+            };
+
+            //act
+            var sut = new Create.Handler(context);
+            var ex = Assert.ThrowsAsync<RestException>(() => sut.Handle(CreateCommandInvalidLocationId, CancellationToken.None));
+
+            var thrownError = ex.Result.Errors.ToString();
+            var expectedError = (new { location = "Could not find location" }).ToString();
+
+            Assert.Equal(expectedError, thrownError);
 
         }
     }
